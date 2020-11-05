75e6849710e3ac810db860664cc40f86a7db08f2
Leandr Khaliullov <l.haliullov@simdev.ru>
2020-11-05 15:53:53 +0400

    fixed iOS reenabling timer

--- src/ios/Insomnia.m
+++ src/ios/Insomnia.m
@@ -23,7 +23,9 @@
   UIApplication* app = [UIApplication sharedApplication];
 
   if([app isIdleTimerDisabled]) {
-    [app setIdleTimerDisabled:false];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+      [app setIdleTimerDisabled:false];
+    });
   }
   CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
   [self.commandDelegate sendPluginResult:result callbackId:callbackId];
